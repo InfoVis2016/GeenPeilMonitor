1b16b784ef5a5e352a387a99366fd095edb1ef50
Gleb <git@gleb.nl>
2016-03-08 17:11:39 +0100

    Updated rendering function of the bar chart.

--- client/app/bar/bar.directives.js
+++ client/app/bar/bar.directives.js
@@ -71,148 +71,91 @@ angular.module('infovisApp')
           scope.parseData(data);
         });
 
-        // Parse meteo data
-        scope.data = {};
-        var parseData = function (data) {
-          var record;
-          var numDays = 0;
-          var totalTemp = 0;
-          var currentYear;
-          var currentMonth;
-          for ( var i = 0; i < data.length; i++ ) {
-            record = data[i];
-            if ( currentYear && parseInt(record.month) !== currentMonth ) {
-              if ( !scope.data[currentYear] ) scope.data[currentYear] = [];
-              scope.data[currentYear].push({
-                key: currentMonth,
-                value: totalTemp / numDays / 10
-              });
-              totalTemp = 0;
-              numDays = 0;
-            }
-            currentYear = parseInt(record.year);
-            currentMonth = parseInt(record.month);
-            totalTemp += parseInt(record.temperature);
-            numDays++;
-          }
-          scope.render();
-        };
-
-        // Render the data into the visualisation
-        scope.render = function () {
-          // If we don't pass any data, return out of the element
-          if ( !scope.data || !scope.data[selectedYear] ) return;
-          var data = scope.data[selectedYear];
-
-          // Remove all previous items before render
-          svg.selectAll('*').remove();
-
-          x.domain(data.map(function(d) { return d.key; }));
-          y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-          // Draw x-axis
-          svg.append('g')
-            .attr('class', 'x axis')
-            .attr('transform', 'translate(0, ' + height + ')')
-            .call(xAxis);
-
-          // Draw y-axis
-          svg.append('g')
-            .attr('class', 'y axis')
-            .call(yAxis);
-
-          // Add label to show bar temperature
-          var label = svg.append('text')
-            .attr('class', 'bar-label-hover')
-            .attr('text-anchor', 'middle')
-            .attr('opacity', 0);
-
-          // Add the bars
-          svg.selectAll('.bar')
-            .data(data)
-            .enter()
-            .append('rect')
-            .attr('class', 'bar')
-            .attr('x', function(d) { return x(d.key); })
-            .attr('width', x.rangeBand())
-            .attr('y', height)
-            .attr('height', 0)
-            .on('mouseover', function(d) {
-              label.attr('x', x(d.key) + 35);
-              label.attr('y', y(d.value) + 20);
-              label.text(Math.round(d.value * 100) / 100 + ' °C');
-              label.transition()
-                .duration(150)
-                .ease('quad')
-                .attr('y', y(d.value) - 10)
-                .attr('opacity', 1);
-            })
-            .on('mouseout', function() {
-              label.attr('x', -500);
-              label.attr('y', -500);
-              label.attr('opacity', 0);
-            })
-            .transition()
-              .delay(function(d, i) { return i * 150; })
-              .duration(150)
-              .ease('quad')
-              .attr('height', function(d) {
-                return height - y(d.value);
-              })
-              .attr('y', function(d) {
-                return y(d.value);
-              });
-
-          // Add labels to the bars
-          svg.selectAll('.bar-label')
-            .data(data)
-            .enter()
-            .append('text')
-            .attr('class', 'bar-label')
-            .attr('text-anchor', 'middle')
-            .attr('opacity', 0)
-            .attr('fill', '#ff4500')
-            .attr('stroke', '#ff4500')
-            .attr('x', function(d) { return x(d.key) + 35; })
-            .attr('y', function (d) { return y(d.value) + 50; })
-            .text(function (d) { return Math.round(d.value * 100) / 100 + ' °C'; })
-            .transition()
-              .delay(function(d, i) { return i * 150; })
-              .duration(350)
-              .ease('quad')
-              .attr('opacity', 1)
-              .attr('y', function(d) {
-                return y(d.value) + 20;
-              });
-
-          // Show currently selected year
-          svg.append('text')
-            .attr('class', 'year-label')
-            .attr('x', width-80)
-            .attr('y', 0)
-            .text(selectedYear);
-
-          // Switching between the years using the arrow keys
-          d3.select('body').on('keyup', function () {
-
-            // Get array of all years used in the data
-            var years = _.map(Object.keys(scope.data), function (x) { return parseInt(x); });
-            var index = years.indexOf(selectedYear);
-
-            // User navigates to the left
-            if ( d3.event.keyCode === 37 ) {
-              selectedYear = index === 0 ? years[years.length-1] : years[index - 1];
-            }
-
-            // User navigates to the right
-            if ( d3.event.keyCode === 39 ) {
-              selectedYear = index === years.length-1 ? years[0] : years[index + 1];
-            }
-
-            // Render the data again
-            scope.render();
+        scope.parseData = function(data) {
+          var clean_data = [];
+          var parseDate = d3.time.format("%d/%m/%Y").parse;
+          _.each(data, function(d) {
+            clean_data.push({
+              date: parseDate(d.date), // turn the date string into a date object
+              total: d.count
+            });
           });
+          scope.render(clean_data);
+        }
+
+        scope.render = function(data) {
+          // data ranges for the x and y axes
+          x.domain(d3.extent(data, function(d) { return d.date; }));
+          y.domain([0, d3.max(data, function(d) { return d.total; })]);
+          xOverview.domain(x.domain());
+          yOverview.domain(y.domain());
+
+          // data range for the bar colours
+          // (essentially maps attribute names to colour values)
+          colour.domain(d3.keys(data[0]));
+
+          // draw the axes now that they are fully set up
+          main.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis);
+          main.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+          overview.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + heightOverview + ")")
+            .call(xAxisOverview);
+
+          // draw the bars
+          main.append("g")
+            .attr("class", "bars")
+            // a group for each stack of bars, positioned in the correct x position
+            .selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("width", 6)
+              //.attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; })
+              .attr('x', function(d) { return x(d.date); })
+              .attr("y", function(d) { return y(d.total); })
+              .attr("height", function(d) { return height - y(d.total); });
+
+          overview.append("g")
+            .attr("class", "bars")
+            .selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", function(d) { return xOverview(d.date) - 3; })
+              .attr("width", 6)
+              .attr("y", function(d) { return yOverview(d.total); })
+              .attr("height", function(d) { return heightOverview - yOverview(d.total); });
+
+          // add the brush target area on the overview chart
+          overview.append("g")
+            .attr("class", "x brush")
+            .call(brush)
+            .selectAll("rect")
+              // -6 is magic number to offset positions for styling/interaction to feel right
+              .attr("y", -6)
+              // need to manually set the height because the brush has
+              // no y scale, i.e. we should see the extent being marked
+              // over the full height of the overview chart
+              .attr("height", heightOverview + 7);  // +7 is magic number for styling
         };
+
+        // zooming/panning behaviour for overview chart
+        function brushed() {
+          // update the main chart's x axis data range
+          x.domain(brush.empty() ? xOverview.domain() : brush.extent());
+
+          // redraw the bars on the main chart
+          main.selectAll(".bar").attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; })
+
+          // redraw the x axis of the main chart
+          main.select(".x.axis").call(xAxis);
+        }
       }
     };
   });
