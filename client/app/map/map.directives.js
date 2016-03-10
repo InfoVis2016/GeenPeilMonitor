8a40c845a8b2e9348489ccd3efa8ab2c39e2238c
K0en <kschnitfink@gmail.com>
2016-03-10 13:22:18 +0100

    Added initial map visualisation.

--- client/app/map/map.directives.js
+++ client/app/map/map.directives.js
@@ -12,29 +12,35 @@ angular.module('infovisApp')
       template: '<div class="map-chart"></div>',
       link: function (scope, element, attrs) {
 
-        var margin = {
-          top: 30,
-          right: 20,
-          bottom: 50,
-          left: 50
-        };
+        var xym = d3.geo.mercator();
+        var path = d3.geo.path().projection(xym);
+        var colors = d3.scale.category20();
 
-        var width = 960 - margin.left - margin.right;
-        var height = 500 - margin.top - margin.bottom;
+        // the variable that holds our translate, centers on the netherlands
+        var translate = xym.translate();
+        translate[0] = -500;
+        translate[1] = 10640
+
+        // center on the netherlands and zoom all the way in
+        xym.translate(translate);
+        xym.scale(60000);
 
         var svg = d3.select(element[0])
-          .append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height', height + margin.top + margin.bottom)
-          .append('g')
-          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+            .append("svg").attr("id","svgoriginal");
 
-        // Show currently selected year
-        svg.append('text')
-          .attr('class', 'label')
-          .attr('x', 0)
-          .attr('y', 0)
-          .text('add map vis here');
+        var gemeentes = svg.append("g")
+            .attr("id", "gemeentes")
+            .attr("class", "RdYlGn");
+
+        d3.json("gem1.json", function (json) {
+            gemeentes.selectAll("path")   // select all the current path nodes
+                .data(json.features)      // bind these to the features array in json
+                .enter().append("path")   // if not enough elements create a new path
+                .attr("fill",function(d,i){return colors(i)})
+                .attr("class", "")  // add attribute class and fill with result from quantize
+                .attr("id", "")
+                .attr("d", path)          // transform the supplied jason geo path to svg
+        });
       }
     };
   });
