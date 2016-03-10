ee3ca153ad136b5115feecffddff9b95e6109c3c
Gleb <git@gleb.nl>
2016-03-10 16:21:11 +0100

    Added colorbrewer to karma and index files.

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,7 @@ module.exports = function(config) {
       'client/bower_components/angular-messages/angular-messages.js',
       'client/bower_components/angular-material/angular-material.js',
       'client/bower_components/d3/d3.js',
+      'client/bower_components/colorbrewer/colorbrewer.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'node_modules/socket.io-client/socket.io.js',
