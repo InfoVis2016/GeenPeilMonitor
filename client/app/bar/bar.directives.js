7f60bc7f2258e6ef87738805fcda72681ea43bb6
Gleb <git@gleb.nl>
2016-03-10 14:35:46 +0100

    Fixed jshint warnings, replaces string quotes, codestyle.

--- client/app/bar/bar.directives.js
+++ client/app/bar/bar.directives.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* globals d3 */
+
 /**
  * Infovis directives
  */
@@ -10,7 +12,7 @@ angular.module('infovisApp')
       restrict: 'E',
       replace: true,
       template: '<div class="bar-chart"></div>',
-      link: function (scope, element, attrs) {
+      link: function (scope, element) {
 
         var margin = {
           top: 30,
@@ -37,41 +39,41 @@ angular.module('infovisApp')
         // rendering for the x and y axes
         var xAxis = d3.svg.axis()
           .scale(x)
-          .orient("bottom");
+          .orient('bottom');
         var yAxis = d3.svg.axis()
           .scale(y)
-          .orient("left");
+          .orient('left');
         var xAxisOverview = d3.svg.axis()
           .scale(xOverview)
-          .orient("bottom");
+          .orient('bottom');
 
         var svg = d3.select(element[0])
           .append('svg')
           .attr('width', width + margin.left + margin.right)
           .attr('height', height + margin.top + margin.bottom);
 
-        var main = svg.append("g")
-          .attr("class", "main")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        var overview = svg.append("g")
-          .attr("class", "overview")
-          .attr("transform", "translate(" + marginOverview.left + "," + marginOverview.top + ")");
+        var main = svg.append('g')
+          .attr('class', 'main')
+          .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        var overview = svg.append('g')
+          .attr('class', 'overview')
+          .attr('transform', 'translate(' + marginOverview.left + ',' + marginOverview.top + ')');
 
         // brush tool to let us zoom and pan using the overview chart
         var brush = d3.svg.brush()
           .x(xOverview)
-          .on("brush", brushed);
+          .on('brush', brushed);
 
         // Get data from server
         d3.json('tweets.json', function(error, data) {
-          if (error) return console.log(error);
+          if (error) { throw error; }
           scope.parseData(data);
         });
 
         scope.parseData = function(data) {
           var seen_dates = [];
           var clean_data = [];
-          var parseDate = d3.time.format("%d/%m/%Y").parse;
+          var parseDate = d3.time.format('%d/%m/%Y').parse;
           _.each(data, function(d) {
             if ( seen_dates.indexOf(d.date) > -1 ) {
               _.find(clean_data, function(cd) {
@@ -87,7 +89,7 @@ angular.module('infovisApp')
             }
           });
           scope.render(clean_data);
-        }
+        };
 
         scope.render = function(data) {
           // data ranges for the x and y axes
@@ -97,53 +99,53 @@ angular.module('infovisApp')
           yOverview.domain(y.domain());
 
           // draw the axes now that they are fully set up
-          main.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + height + ")")
+          main.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
             .call(xAxis);
-          main.append("g")
-            .attr("class", "y axis")
+          main.append('g')
+            .attr('class', 'y axis')
             .call(yAxis);
-          overview.append("g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + heightOverview + ")")
+          overview.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + heightOverview + ')')
             .call(xAxisOverview);
 
           // draw the bars
-          main.append("g")
-            .attr("class", "bars")
+          main.append('g')
+            .attr('class', 'bars')
             // a group for each stack of bars, positioned in the correct x position
-            .selectAll(".bar")
+            .selectAll('.bar')
             .data(data)
-            .enter().append("rect")
-              .attr("class", "bar")
-              .attr("width", 10)
+            .enter().append('rect')
+              .attr('class', 'bar')
+              .attr('width', 10)
               .attr('x', function(d) { return x(d.date); })
-              .attr("y", function(d) { return y(d.total); })
-              .attr("height", function(d) { return height - y(d.total); });
+              .attr('y', function(d) { return y(d.total); })
+              .attr('height', function(d) { return height - y(d.total); });
 
-          overview.append("g")
-            .attr("class", "bars")
-            .selectAll(".bar")
+          overview.append('g')
+            .attr('class', 'bars')
+            .selectAll('.bar')
             .data(data)
-            .enter().append("rect")
-              .attr("class", "bar")
-              .attr("x", function(d) { return xOverview(d.date) - 3; })
-              .attr("width", 6)
-              .attr("y", function(d) { return yOverview(d.total); })
-              .attr("height", function(d) { return heightOverview - yOverview(d.total); });
+            .enter().append('rect')
+              .attr('class', 'bar')
+              .attr('x', function(d) { return xOverview(d.date) - 3; })
+              .attr('width', 6)
+              .attr('y', function(d) { return yOverview(d.total); })
+              .attr('height', function(d) { return heightOverview - yOverview(d.total); });
 
           // add the brush target area on the overview chart
-          overview.append("g")
-            .attr("class", "x brush")
+          overview.append('g')
+            .attr('class', 'x brush')
             .call(brush)
-            .selectAll("rect")
+            .selectAll('rect')
               // -6 is magic number to offset positions for styling/interaction to feel right
-              .attr("y", -6)
+              .attr('y', -6)
               // need to manually set the height because the brush has
               // no y scale, i.e. we should see the extent being marked
               // over the full height of the overview chart
-              .attr("height", heightOverview + 7);  // +7 is magic number for styling
+              .attr('height', heightOverview + 7);  // +7 is magic number for styling
         };
 
         // zooming/panning behaviour for overview chart
@@ -152,10 +154,10 @@ angular.module('infovisApp')
           x.domain(brush.empty() ? xOverview.domain() : brush.extent());
 
           // redraw the bars on the main chart
-          main.selectAll(".bar").attr("transform", function(d) { return "translate(" + x(d.date) + ",0)"; })
+          main.selectAll('.bar').attr('transform', function(d) { return 'translate(' + x(d.date) + ',0)'; });
 
           // redraw the x axis of the main chart
-          main.select(".x.axis").call(xAxis);
+          main.select('.x.axis').call(xAxis);
         }
       }
     };
