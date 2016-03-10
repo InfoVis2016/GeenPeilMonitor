722cb5b655a323d17b91947f661cade84946a8b1
Gleb <git@gleb.nl>
2016-03-10 14:14:34 +0100

    Stroke color for the polygons.

--- client/app/map/map.directives.js
+++ client/app/map/map.directives.js
@@ -44,7 +44,7 @@ angular.module('infovisApp')
                 .data(json.features)      // bind these to the features array in json
                 .enter().append("path")   // if not enough elements create a new path
                 .attr("fill",function(d,i){return colors(i)})
-                .attr("stroke", "#333")
+                .attr("stroke", "#fff")
                 .attr("class", "gemeente")  // add attribute class and fill with result from quantize
                 .attr("id", function(d) { return d.properties.GM_CODE; })
                 .attr("d", path)          // transform the supplied jason geo path to svg
