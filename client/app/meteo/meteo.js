488549dc04b2df7d5166a044d587dab39a1923a9
Gleb <git@gleb.nl>
2016-03-14 14:07:17 +0100

    Added meteo visualisation module.

---
+++ client/app/meteo/meteo.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('infovisApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('meteo', {
+        url: '/meteo',
+        templateUrl: 'app/meteo/meteo.html',
+        controller: 'MeteoCtrl'
+      });
+  });
