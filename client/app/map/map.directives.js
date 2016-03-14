3a653bd45e1734e7fd2f1f2d407d489beec0535a
Gleb <git@gleb.nl>
2016-03-14 11:06:47 +0100

    Set no-select class on the info container to disable user-selections.

--- client/app/map/map.directives.js
+++ client/app/map/map.directives.js
@@ -39,7 +39,7 @@ angular.module('infovisApp')
 
         var info = svg.append('g')
           .attr('opacity', 0)
-          .attr('class', 'info');
+          .attr('class', 'info no-select');
         var info_title = info.append('text')
           .attr('x', 800)
           .attr('y', 150)
