586eada6f2810d0f37f395969764273674f1f09e
Gleb <git@gleb.nl>
2016-03-21 15:29:45 +0100

    Refactored d3 functions in a proper modular setup making use of the scope var for name space.

--- client/app/cloud/cloud.directives.js
+++ client/app/cloud/cloud.directives.js
@@ -19,7 +19,43 @@ angular.module('infovisApp')
         var width = 960;
         var height = 500;
 
-        function draw(words) {
+        // Get data from server
+        d3.json('words.json', function(error, data) {
+          if (error) { throw error; }
+          scope.setup(data);
+        });
+
+        /**
+         * Setup how to draw word cloud
+         * @param data List of objects with words and sizes
+         */
+        scope.setup = function (data) {
+          var sizeScale = d3.scale.linear()
+            .domain([0, d3.max(data, function(d) {
+              return d.size;
+            })])
+            .range([0, width / 1.5]);
+
+          var layout = d3.layout.cloud()
+            .size([width, height])
+            .padding(5)
+            .rotate(function() {
+              return ~~(Math.random() * 2) * 90;
+            })
+            .font('Impact')
+            .fontSize(function(d) {
+              return sizeScale(d.size);
+            })
+            .on('end', scope.draw);
+          layout.words(data);
+          layout.start();
+        };
+
+        /**
+         * Draw function to actually draw the cloud
+         * @param words Array of words to draw in a cloud
+         */
+        scope.draw = function (words) {
           d3.select(element[0])
             .append('svg')
             .attr('preserveAspectRatio', 'xMinYMin meet')
@@ -42,32 +78,7 @@ angular.module('infovisApp')
                 return 'translate(' + [d.x, d.y] + ')rotate(' + d.rotate + ')';
               })
               .text(function(d) { return d.text; });
-        }
-
-        // Get data from server
-        d3.json('words.json', function(error, data) {
-          if (error) { throw error; }
-          
-          var sizeScale = d3.scale.linear()
-            .domain([0, d3.max(data, function(d) {
-              return d.size;
-            })])
-            .range([0, width / 1.5]);
-
-          var layout = d3.layout.cloud()
-          	.size([width, height])
-          	.padding(5)
-          	.rotate(function() {
-            	return ~~(Math.random() * 2) * 90;
-          	})
-          	.font('Impact')
-          	.fontSize(function(d) {
-            	return sizeScale(d.size);
-          	})
-          	.on('end', draw);        
-          layout.words(data);
-          layout.start();
-        });
+        };
       }
     };
   });
