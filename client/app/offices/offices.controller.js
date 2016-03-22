b5320c00b8381ef3897db19051e008ff4a52b186
Gleb <git@gleb.nl>
2016-03-22 23:02:52 +0100

    Added initial voting offices module.

---
+++ client/app/offices/offices.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function() {
+
+class OfficesController {
+
+  constructor() {
+  }
+
+}
+
+angular.module('infovisApp')
+  .controller('OfficesController', OfficesController);
+
+})();
